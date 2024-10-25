ba27cd8e35e31b541fba6ab0ae4e4ab7ac602d19
11Rojas <119141741+11Rojas@users.noreply.github.com>
2024-10-25 06:31:39 -0400

    Add files via upload

---
+++ src/app/components/Background.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+
+const ParticlesBackground: React.FC = () => {
+
+  return (
+    <Particles />
+)
+};
+
+export default ParticlesBackground;
